cbe251587329eeac526e3807920ba17dbf2abff5
Simon A. Eugster <simon.eu@gmail.com>
2011-03-02 12:26:55 +0100

    always same format for version numbers

--- version.sh
+++ version.sh
@@ -3,7 +3,7 @@ cd $(dirname $0)
 now=$(date +%F)
 revision=$(git describe --always)
 version=$(echo "${now}-${revision}")
-version=$(echo $(git describe --always HEAD)+$(git rev-list HEAD -n 1 |cut -c 1-7))
+version=$(echo $(git describe --always HEAD --abbrev=0)+$(git rev-list HEAD -n 1 |cut -c 1-7))
 #echo ${now}
 #echo ${revision}
 echo ${version}
