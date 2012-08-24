Granjow/wiki2xhtml

commit 164ee5b7c699379196d936271ced3bb774360efe

Custom image arguments can have any name («var» prefix not forced anymore).
This also deprecates some built-in variables like caption=, ld=, etc
which are better handled by the template directly.

src/project/settings/ImageProperties.java
@@ -269,7 +269,7 @@ public class ImageProperties extends StringSettings<EImageProperties> {
 		Matcher mSize;
 		Pattern pImage = Pattern.compile("(?i)(?:image|bild|file):(.+)");
 		Matcher mPImage;
-		Pattern pUserVar = Pattern.compile("(?i)(var[a-z0-9_-]+)=(.+)");
+		Pattern pUserVar = Pattern.compile("(?i)([a-z][a-z0-9_-]+)=(.+)");
 		Matcher mUserVar;
 		Pattern pCaption = Pattern.compile("(?i)(?:c(?:aption)?=)(.+)");
 		Matcher mCaption;
@@ -332,57 +332,34 @@ public class ImageProperties extends StringSettings<EImageProperties> {
 				continue;
 			}
 			
+
+			// Deprecated //
+			if (ai.fullArg.startsWith(Constants.Template_Images.link + SEP)) {
+				parentFile.project.deprecator.removedForTemplateMessage("link= argument for Images", parentFile);
+			}
+			if (ai.fullArg.startsWith(Constants.Template_Images.alt + SEP)) {
+				parentFile.project.deprecator.removedForTemplateMessage("alt= argument for Images", parentFile);
+			}
+			mCaption = pCaption.matcher(ai.fullArg);
+			if (mCaption.matches()) {
+				parentFile.project.deprecator.removedForTemplateMessage("caption= and c= argument for Images", parentFile);
+			}
+			if (Constants.Template_Images.noscale.equals(ai.fullArg)) {
+				parentFile.project.deprecator.removedForTemplateMessage("noscale argument for Images", parentFile);
+			}
+			if (Constants.Template_Images.clear.equals(ai.fullArg)) {
+				parentFile.project.deprecator.removedForTemplateMessage("clear argument for Images", parentFile);
+			}
+			if (ai.fullArg.startsWith("ld" + SEP)) {
+				parentFile.project.deprecator.removedForTemplateMessage("ld= argument for Images", parentFile);
+			}
+			
 			mUserVar = pUserVar.matcher(ai.fullArg);
 			if (mUserVar.find()) {
 				// varSomething=myVar
 				// User defined parameter. The parameter name has to start with «var» and may only contain
 				// valid characters; See pUserVar.
 				props.put(mUserVar.group(1), mUserVar.group(2));
-				continue;
-			}
-			
-			if (ai.fullArg.startsWith(Constants.Template_Images.link + SEP)) {
-				// link=<custom link>
-				props.put(Constants.Template_Images.link, ai.fullArg.substring(Constants.Template_Images.link.length()+1));
-				continue;
-			}
-			
-			if (ai.fullArg.startsWith(Constants.Template_Images.alt + SEP)) {
-				// alt=<alternative text>
-				props.put(Constants.Template_Images.alt, ai.fullArg.substring(Constants.Template_Images.alt.length()+1));
-				continue;
-			}
-			
-			mCaption = pCaption.matcher(ai.fullArg);
-			if (mCaption.matches()) {
-				// caption=<image caption>
-				props.put(Constants.Template_Images.caption, mCaption.group(1));
-				continue;
-			}
-
-
-			if (Constants.Template_Images.noscale.equals(ai.fullArg)) {
-				// noscale
-				// Indicates that the image should not be scaled on the image page
-				props.put(Constants.Template_Images.noscale, "true");
-				continue;
-			}
-			
-			if (Constants.Template_Images.clear.equals(ai.fullArg)) {
-				// clear
-				props.put(Constants.Template_Images.clear, "both");
-				continue;
-			}
-			if (ai.fullArg.startsWith(Constants.Template_Images.clear + SEP)) {
-				// clear=<left/right>
-				props.put(Constants.Template_Images.clear, ai.fullArg.substring(Constants.Template_Images.clear.length() + 1));
-				continue;
-			}
-			
-
-			if (ai.fullArg.startsWith("ld" + SEP)) {
-				// ld=<long description>
-				props.put(Constants.Template_Images.longDesc, ai.fullArg.substring(3));
 				continue;
 			}
 

unittests/PicTester.java
@@ -84,8 +84,8 @@ public class PicTester extends junit.framework.TestCase {
 		final String template1 = "{{#if:{{{varUser1|}}}|u1={{{varUser1}}}}}";
 		assertEquals("u1=hello wiki!", p("[[Image:img.jpg|varUser1=hello wiki!]]", template1));
 		
-		final String template2 = "<a href=\"{{{path}}}\"{{#if:{{{varLinkArg|}}}| {{{varLinkArg}}}}}>img</a>";
-		assertEquals("<a href=\"img.jpg\" rel=\"shadowbox[abc]\">img</a>", p("[[Image:img.jpg|varLinkArg=rel=\"shadowbox[abc]\"]]", template2));
+		final String template2 = "<a href=\"{{{path}}}\"{{#if:{{{linkArg|}}}| {{{linkArg}}}}}>img</a>";
+		assertEquals("<a href=\"img.jpg\" rel=\"shadowbox[abc]\">img</a>", p("[[Image:img.jpg|linkArg=rel=\"shadowbox[abc]\"]]", template2));
 	}
 	
 	@Test
@@ -96,37 +96,11 @@ public class PicTester extends junit.framework.TestCase {
 	}
 	
 	@Test
-	public void testAlt() throws IOException {
-		final String template = "{{#if:{{{alt|}}}|alt={{{alt}}}}}";
-		assertEquals("alt=nothing", p("[[Image:img.jpg|alt=nothing]]", template));
-	}
-	
-	@Test
-	public void testSmall() throws IOException {
-		final String template = "{{#if:{{{noscale|}}}|true|false}}";
-		assertEquals("true", p("[[Image:img.jpg|noscale]]", template));
-		assertEquals("false", p("[[Image:img.jpg]]", template));
-	}
-	
-	@Test
-	public void testLongdesc() throws IOException {
-		final String template = "{{#if:{{{longdesc|}}}|ld:{{{longdesc}}}}}";
-		assertEquals("ld:Long description.", p("[[Image:img.jpg|ld=Long description.]]", template));
-	}
-	
-	@Test
 	public void testClear() throws IOException {
 		final String template = "{{#if:{{{clear|}}}|clear:{{{clear}}}}}";
-		assertEquals("clear:both", p("[[Image:img.jpg|clear]]", template));
+		assertEquals("clear:both", p("[[Image:img.jpg|clear=both]]", template));
 		assertEquals("clear:left", p("[[Image:img.jpg|clear=left]]", template));
 		assertEquals("clear:right", p("[[Image:img.jpg|clear=right]]", template));
-	}
-	
-	@Test
-	public void testCaption() throws IOException {
-		final String template = "{{#if:{{{caption|}}}|cap={{{caption}}}}}";
-		assertEquals("cap=image title", p("[[Image:img.jpg|caption=image title]]", template));
-		assertEquals("cap=image title", p("[[Image:img.jpg|c=image title]]", template));
 	}
 	
 	

src/Constants.java
@@ -260,12 +260,14 @@ public final class Constants {
 	/** Arguments available for image templates (i.e. image and gallery entries). See {@link WikiImages}. */
 	public static final class Template_Images {
 
-		/** Clear before and after (insert clear=both) to put it on a new line */
+		/** Clear before and after (insert clear=both) to put it on a new line
+		 * @deprecated */
 		public static final String clear = "clear";
 		/** <p>Don't create an image page, link directly.</p>
 		 * <p>contains {@code "true"} (i.e. is not empty) if this property is set.</p> */
 		public static final String direct = "direct";
-		/** Do not scale the image on the image page. */
+		/** Do not scale the image on the image page.
+		 * @deprecated */
 		public static final String noscale = "noscale";
 		/**This argument has two different meanings (which don't imply each other): 
 		 * <ol>
@@ -292,13 +294,16 @@ public final class Constants {
 		/** Horizontal position. Can be one of left, center, and right. */
 		public static final String location = "location";
 		/** Argument: Custom link to use.<br/>
-		 * Template: Target link.*/
+		 * Template: Target link.
+		 * @deprecated */
 		public static final String link = "link";
 		/** Image ID for linking/anchoring */
 		public static final String id = "id";
-		/** Image caption */
+		/** Image caption
+		 * @deprecated */
 		public static final String caption = "caption";
-		/** Alternative description (if the image cannot be displayed). */
+		/** Alternative description (if the image cannot be displayed).
+		 * @deprecated */
 		public static final String alt = "alt";
 		
 		/** Number of the image on the current page */
@@ -312,7 +317,8 @@ public final class Constants {
 		 * <p>Available in the template only.</p> */
 		public static final String rowEnd = "rowEnd";
 
-		/** Long description of the image */
+		/** Long description of the image
+		 * @deprecated */
 		public static final String longDesc = "longdesc";
 		
 		/** To comment out a gallery entry. Lines starting with this prefix are ignored. */

src/commentator/Deprecator.java
@@ -1,0 +1,28 @@
+package src.commentator;
+import src.project.file.WikiFile;
+
+public class Deprecator {
+
+	public Deprecator() {}
+	
+	public boolean onlySummary = true;
+	
+	private StringBuffer allRemovedForTemplate = new StringBuffer(); 
+	
+	public void removedForTemplateMessage(String warning, WikiFile source) {
+		if (!onlySummary) {
+			System.err.println("In " + source.projectAbsoluteName() + ":");
+			System.err.println("UNSUPPORTED: " + warning);
+			System.err.println("Please use arguments via the templating function directly.");
+		}
+		
+		allRemovedForTemplate.append("In " + source.projectAbsoluteName() + ": " + warning + "\n");
+	}
+	
+	public void printAllWarnings()
+	{
+		System.err.println("The following arguments are now UNSUPPORTED in wiki2xhtml and templates:\n" + allRemovedForTemplate);
+	}
+	
+
+}

src/project/WikiProject.java
@@ -25,6 +25,7 @@ import java.util.Vector;
 import src.Constants;
 import src.Statistics;
 import src.Wiki2xhtmlArgsParser;
+import src.commentator.Deprecator;
 import src.project.FallbackFile.FallbackLocation;
 import src.project.FallbackFile.NoFileFoundException;
 import src.project.file.VirtualWikiFile;
@@ -237,6 +238,8 @@ public class WikiProject {
 		sitemap.write();
 		
 		Statistics.getInstance().sw.timeOverall.stop();
+		
+		deprecator.printAllWarnings();
 		System.out.printf("Total time taken: %s\n", Statistics.getInstance().sw.timeOverall.getStoppedTimeString());
 	}
 	
@@ -302,6 +305,8 @@ public class WikiProject {
 	public Sitemap sitemap = null;
 	public WikiStyle wikiStyle = new WikiStyle(this);
 	public Wiki2xhtmlArgsParser argsParser = null;
+	
+	public Deprecator deprecator = new Deprecator();
 	
 	// Settings
 	private Settings<SettingsE, String> projectSettings = new PageSettings();

src/tasks/WikiImages.java
@@ -43,9 +43,9 @@ import src.utilities.IOWrite_Stats;
  * <p>Inserts images.</p>
  * <p>Example invocation:</p>
  * <p><code>[[Image:path/to/image.jpg|thumb|left|Description]]</code></p>
- * <p>User-defined arguments (can be used in the template for the image page, see {@link Templates})
- * <em>must</em> start with <code>var</code>; See the function {@link ImageProperties#readArguments(String)}. Example:</p>
- * <p><code>[[Image:mainGUI.png|varCustomID=mainGUI]]</code></p>
+ * <p>User-defined arguments can be used in the template for the image page, see {@link Templates} and
+ * the function {@link ImageProperties#readArguments(String)}. Example:</p>
+ * <p><code>[[Image:mainGUI.png|customID=mainGUI]]</code></p>
  * <p>(This could e.g. be used together with an according image template that checks this name and uses its value 
  * as ID if it is given. Simplifies linking to an image from a different place without having to insert the (often changing)
  * generated image ID.)</p> 

commit 4b179892cb7301484fafa1e8bfc0f03c4f0789cb

Fix image parameter recognition: Key must not contain spaces.
Bug introduced in: 164ee5b7c699379196d936271ced3bb774360efe

src/project/settings/ImageProperties.java
@@ -269,7 +269,7 @@ public class ImageProperties extends StringSettings<EImageProperties> {
 		Matcher mSize;
 		Pattern pImage = Pattern.compile("(?i)(?:image|bild|file):(.+)");
 		Matcher mPImage;
-		Pattern pUserVar = Pattern.compile("(?i)([a-z][a-z0-9_-]+)=(.+)");
+		Pattern pUserVar = Pattern.compile("(?i)^([a-z][a-z0-9_-]*)=(.+)");
 		Matcher mUserVar;
 		Pattern pCaption = Pattern.compile("(?i)(?:c(?:aption)?=)(.+)");
 		Matcher mCaption;
@@ -356,7 +356,7 @@ public class ImageProperties extends StringSettings<EImageProperties> {
 			
 			mUserVar = pUserVar.matcher(ai.fullArg);
 			if (mUserVar.find()) {
-				// varSomething=myVar
+				// key=var
 				// User defined parameter. The parameter name has to start with «var» and may only contain
 				// valid characters; See pUserVar.
 				props.put(mUserVar.group(1), mUserVar.group(2));

unittests/PicTester.java
@@ -96,6 +96,15 @@ public class PicTester extends junit.framework.TestCase {
 	}
 	
 	@Test
+	public void testLinks() throws IOException {
+		final String template = "{{#if:{{{text|}}}|text={{{text}}}}}";
+		assertEquals("text=this is a [[link]]", p("[[Image:img.jpg|this is a [[link]]]]", template));
+		assertEquals("text=The <a href=\"http://vimeo.com/10756036\" class=\"external\">Rusto Fat</a>.", 
+				pl("[[Image:rusto-fat.jpg|The <a href=\"http://vimeo.com/10756036\" class=\"external\">Rusto Fat</a>.]]", template));
+		
+	}
+	
+	@Test
 	public void testClear() throws IOException {
 		final String template = "{{#if:{{{clear|}}}|clear:{{{clear}}}}}";
 		assertEquals("clear:both", p("[[Image:img.jpg|clear=both]]", template));
@@ -117,22 +126,36 @@ public class PicTester extends junit.framework.TestCase {
 		to.writeFile("tplImage.txt", template);
 		return to.real();
 	}
+	private static final String pl(String testString, String template) throws IOException {
+		TestObject to = new TestObject(testString, "", true);
+		to.writeFile("tplImage.txt", template);
+		return to.real();
+	}
 	private static final String p(String testString) throws IOException {
 		final String template1 = "<img src=\"{{{path}}}\"/>";
 		return p(testString, template1);
 	}
 	
 	private static class TestObject extends unittests.TestObject {
+		
+		private final boolean withLinks;
 		public TestObject(String test, String result) throws IOException {
+			this(test, result, false);
+		}
+		public TestObject(String test, String result, boolean withLinks) throws IOException {
 			super(test, result);
+			this.withLinks = withLinks;
 		}
 
 		void fillTasks(VirtualWikiFile vf) {
 			vf.addTask(Task.Images);
+			if (withLinks) {
+				vf.addTask(Task.Links);
+			}
 		}
 
 		public String real() throws IOException {
-			return super.real().replace("\n", "").replace(" <", "<");
+			return super.real().replace("\n", "");
 		}
 	}
 	private static class TestObject2 extends TestObject {